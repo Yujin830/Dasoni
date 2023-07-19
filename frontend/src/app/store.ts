195132bde312f329aa3c0cb12c66bf732a3a5541
zero9657@naver.com <zero9657@naver.com>
2023-07-19 17:49:49 +0900

    feat : 로그인 입력값 redux store에 저장

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,14 +1,16 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// import counterReducer from '../features/counter/counterSlice';
+import user from './modules/user';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user,
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export default store;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
