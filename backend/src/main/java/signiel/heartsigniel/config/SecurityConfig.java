07b5037cddcd7e429aaa57073112d17fd67ae999
rladbwls30 <rladbwls30@naver.com>
2023-07-24 17:44:21 +0900

    Feat: 로그인, 회원가입 대충 완성

--- backend/src/main/java/signiel/heartsigniel/config/SecurityConfig.java
+++ backend/src/main/java/signiel/heartsigniel/config/SecurityConfig.java
@@ -9,10 +9,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import signiel.heartsigniel.Jwt.JwtAuthenticationFilter;
 import signiel.heartsigniel.Jwt.JwtTokenProvider;
+
+import javax.naming.AuthenticationException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,17 +34,60 @@ public class SecurityConfig {
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
+                // ID, Password 문자열을 Base64로 인코딩하여 전달하는 구조
                 .httpBasic().disable()
+                // 쿠키 기반이 아닌 JWT 기반이므로 사용하지 않음
                 .csrf().disable()
+                // CORS 설정
+                .cors(c -> {
+                            CorsConfigurationSource source = request -> {
+                                // Cors 허용 패턴
+                                CorsConfiguration config = new CorsConfiguration();
+                                config.setAllowedOrigins(
+                                        List.of("*")
+                                );
+                                config.setAllowedMethods(
+                                        List.of("*")
+                                );
+                                return config;
+                            };
+                            c.configurationSource(source);
+                        }
+                )
+                // Spring Security 세션 정책 : 세션을 생성 및 사용하지 않음
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                // 조건별로 요청 허용/제한 설정
                 .authorizeRequests()
-                .antMatchers("/users/login").permitAll()
-                .antMatchers("/users/regist").permitAll()
-                .antMatchers("/users/test").hasRole("USER")
-                .anyRequest().authenticated()
+                // 회원가입과 로그인은 모두 승인
+                .antMatchers("/register", "/login").permitAll()
+                // /users 로 시작하는 요청은 USER 권한이 있는 유저에게만 허용
+                .antMatchers("/users/**").hasRole("USER")
+                .anyRequest().denyAll()
                 .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+                // JWT 인증 필터 적용
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                // 에러 핸들링
+                .exceptionHandling()
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest request, HttpServletResponse response, org.springframework.security.access.AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                        response.setStatus(403);
+                        response.setCharacterEncoding("utf-8");
+                        response.setContentType("text/html; charset=UTF-8");
+                        response.getWriter().write("권한이 없는 사용자입니다.");
+                    }
+                })
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
+                        response.setStatus(401);
+                        response.setCharacterEncoding("utf-8");
+                        response.setContentType("text/html; charset=UTF-8");
+                        response.getWriter().write("인증되지 않은 사용자입니다.");
+                    }
+                });
+
         return http.build();
     }
 
