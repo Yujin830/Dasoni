8da658a8f48cc4bcc7da16cfeb467b9ab584ecb6
zero9657@naver.com <zero9657@naver.com>
2023-07-26 10:56:48 +0900

    feat : 데이터 유효성 검사 로직 추가

--- frontend/src/components/MyPage/ChangePassword/MyProfileChangePw.tsx
+++ frontend/src/components/MyPage/ChangePassword/MyProfileChangePw.tsx
@@ -28,17 +28,24 @@ const styles = {
   },
 };
 
+// 현재 비밀번호 확인 컴포넌트 :: 이중 인증 목적
 function CheckCurrentPw({ setIsCorrect, memberId }: any) {
   const [currentPw, setCurrentPw] = useState('');
+  const [alertMsg, setAlertMsg] = useState('');
   const handleCurrentPw = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCurrentPw(e.target.value);
   };
   const handleClick = async () => {
     console.log(currentPw);
-    const res = await axios.post(`http://localhost:8080/users/${memberId}/password`);
+    if (!currentPw) {
+      setAlertMsg('비밀번호를 입력해주세요');
+      return;
+    }
+    const res = await axios.post(`/users/${memberId}/password`);
 
     if (res.data) {
       setIsCorrect(true);
+      setAlertMsg('');
     }
   };
 
@@ -52,14 +59,17 @@ function CheckCurrentPw({ setIsCorrect, memberId }: any) {
         placeholer="현재 비밀번호를 입력해주세요"
         handleChange={handleCurrentPw}
       />
+      <div className={`alert ${alertMsg ? 'show' : ''}`}>{alertMsg}</div>
       <FilledButton style={styles.button} content="확인" handleClick={handleClick} />
     </div>
   );
 }
 
+// 비밀번호 변경 컴포넌튼
 function ChangePw({ setType, memberId }: any) {
   const [newPw, setNewPw] = useState('');
   const [checkNewPw, setCheckNewPw] = useState('');
+  const [alertMsg, setAlertMsg] = useState('');
 
   const handleNewPwChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewPw(e.target.value);
@@ -71,12 +81,23 @@ function ChangePw({ setType, memberId }: any) {
 
   const sendNewPw = async () => {
     // TODO : 비밀번호 유효성 검사
+    console.log(newPw);
     console.log(checkNewPw);
-    const res = await axios.post(`http://localhost:8080/users/${memberId}/password`);
+
+    if (newPw.length < 8 || newPw.length > 10) {
+      setAlertMsg('비밀번호를 8자 이상 10자 이하로 작성해주세요');
+      return;
+    } else if (newPw !== checkNewPw) {
+      setAlertMsg('비밀번호가 일치하지 않습니다. 다시 확인해주세요');
+      return;
+    }
+
+    const res = await axios.post(`/users/${memberId}/password`);
     console.log(res.data);
 
     if (res.data === 'OK') {
       alert(`비밀번호가 성공적으로 변경되었습니다.`);
+      setAlertMsg('');
       setType('read');
     }
   };
@@ -99,6 +120,7 @@ function ChangePw({ setType, memberId }: any) {
         placeholer="다시 한 번 비밀번호를 입력해주세요"
         handleChange={handleCheckNewPwChange}
       />
+      <div className={`alert ${alertMsg ? 'show' : ''}`}>{alertMsg}</div>
       <FilledButton style={styles.button} content="확인" handleClick={sendNewPw} />
     </div>
   );
