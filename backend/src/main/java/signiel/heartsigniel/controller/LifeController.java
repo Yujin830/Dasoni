386b0d2b893a3d9e07d241c4b5e5d7597e9b5653
rladbwls30 <rladbwls30@naver.com>
2023-07-27 10:18:52 +0900

    Feat: 목숨 사용시 insert 완료

---
+++ backend/src/main/java/signiel/heartsigniel/controller/LifeController.java
@@ -0,0 +1,25 @@
+package signiel.heartsigniel.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import signiel.heartsigniel.model.guide.Guide;
+import signiel.heartsigniel.model.guide.GuideService;
+import signiel.heartsigniel.model.life.LifeService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class LifeController {
+    private final LifeService lifeService;
+    @PostMapping("/users/{memberId}")
+    public ResponseEntity<String> insert(@PathVariable Long memberId) throws Exception{
+        return new ResponseEntity<>(lifeService.insert(memberId), HttpStatus.OK);
+    }
+}
