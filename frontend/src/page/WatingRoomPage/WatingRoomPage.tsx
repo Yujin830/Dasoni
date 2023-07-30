9b97dc648aa51cb362f5d88829f802e2bdc90067
KimSeonHui <zero9657@naver.com>
2023-07-31 00:27:36 +0900

    feat : WaitingRoom 페이지 UI 개발

---
+++ frontend/src/page/WatingRoomPage/WatingRoomPage.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import Header from '../../components/Header/Header';
+import titleLogo from '../../assets/image/title_img.png';
+import { WaitingMember } from '../../apis/response/waitingRoomRes';
+import WaitingMemberBox from '../../components/WatingMember/WaitingMember';
+import FilledButton from '../../components/Button/FilledButton';
+import { useNavigate } from 'react-router';
+import './WaitingRoomPage.css';
+
+const watingMember: WaitingMember[] = [
+  {
+    memberId: 1,
+    nickname: 'strong',
+    profileSrc: 'rank_profile.png',
+    gender: 'male',
+    matchCnt: 56,
+    points: 1800,
+  },
+  {
+    memberId: 2,
+    nickname: 'strong2',
+    profileSrc: 'rank_profile.png',
+    gender: 'male',
+    matchCnt: 56,
+    points: 1800,
+  },
+  {
+    memberId: 3,
+    nickname: 'strong3',
+    profileSrc: 'rank_profile.png',
+    gender: 'male',
+    matchCnt: 56,
+    points: 1800,
+  },
+  {
+    memberId: 4,
+    nickname: 'strong',
+    profileSrc: 'rank_profile.png',
+    gender: 'female',
+    matchCnt: 56,
+    points: 1800,
+  },
+  {
+    memberId: 5,
+    nickname: 'strong',
+    profileSrc: 'rank_profile.png',
+    gender: 'female',
+    matchCnt: 56,
+    points: 1800,
+  },
+  {
+    memberId: 6,
+    nickname: 'strong',
+    profileSrc: 'rank_profile.png',
+    gender: 'female',
+    matchCnt: 56,
+    points: 1800,
+  },
+];
+
+const styles = {
+  startBtn: {
+    width: '10rem',
+    height: '4rem',
+    borderRadius: ' 0.5rem',
+    background: '#ECC835',
+    color: '#fff',
+    fontSize: '1.5rem',
+    fontWeight: '700',
+  },
+  exitBtn: {
+    width: '10rem',
+    height: '4rem',
+    borderRadius: ' 0.5rem',
+    background: '#EC5E98',
+    color: '#fff',
+    fontSize: '1.5rem',
+    fontWeight: '700',
+  },
+};
+
+function WaitingRoomPage() {
+  const [memberList, setMemberList] = useState<WaitingMember[]>(watingMember);
+  const navigate = useNavigate();
+
+  const handleStartBtn = () => {
+    alert('미팅이 3초 후 시작됩니다');
+    setTimeout(() => {
+      navigate('/meeting');
+    }, 3000);
+  };
+
+  const handleExitBtn = () => {
+    alert('방 나가기');
+    navigate('/main');
+  };
+
+  return (
+    <div id="waiting-page">
+      <Header />
+      <main id="waiting-room-box">
+        <div id="waiting-room-top">
+          <div className="title">
+            <img src={titleLogo} alt="하트 이미지" />
+            재밌게 놀아요
+          </div>
+          <div className="info">
+            <span>메기 : No</span>
+            <span>Rank : Yellow</span>
+          </div>
+        </div>
+        <div id="waiting-room-content">
+          {memberList.map((member) => (
+            <WaitingMemberBox key={member.memberId} />
+          ))}
+        </div>
+        <div id="waiting-room-footer">
+          <FilledButton content="시작하기" style={styles.startBtn} handleClick={handleStartBtn} />
+          <FilledButton content="나가기" style={styles.exitBtn} handleClick={handleExitBtn} />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default WaitingRoomPage;
