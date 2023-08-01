87d17de4bfafd78c3fa9438807ded65ffad53df9
zero9657@naver.com <zero9657@naver.com>
2023-08-01 15:23:33 +0900

    feat : MeetingPage에 openvidu 참가 video 띄우기 추가

--- frontend/src/page/MeetingPage/MeetingPage.tsx
+++ frontend/src/page/MeetingPage/MeetingPage.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useOpenvidu } from '../../hooks/useOpenvidu';
 import { useParams } from 'react-router';
-import Header from '../../components/Header/Header';
+import UserVideo from '../../components/Session/UserVideo/UserVideo';
+import './MeetingPage.css';
 
 function MeetingPage() {
   const { roomId } = useParams();
   const { publisher, streamList, onChangeCameraStatus, onChangeMicStatus } = useOpenvidu(
-    1,
+    1, // TODO : 로그인한 멤버의 memberId로 store에서 가져오기
     roomId !== undefined ? roomId : '1',
   );
   console.log(publisher);
   console.log(streamList);
+
+  const memberList = useMemo(() => streamList, [streamList]);
   return (
     <div id="meeting">
-      <Header />
-      <p>미팅 페이지</p>
+      <div id="meeting-video-container">
+        {publisher &&
+          memberList.map((stream, index) => (
+            // TODO : 로그인한 멤버의 nickname store에서 가져와서 보여주기
+            <UserVideo streamManager={stream.streamManager} nickname="sunsunny" key={index} />
+          ))}
+      </div>
     </div>
   );
 }
