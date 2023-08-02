0bf4be65f06d0e4b5606b4acf239911e14fc978f
이명우 <dlauddndlek@naver.com>
2023-08-02 10:22:26 +0900

    feat: 생명 사용 및 삭제 로직 + 룸 연동 #BACK-302 Related to : #302

--- backend/src/main/java/signiel/heartsigniel/model/life/LifeService.java
+++ backend/src/main/java/signiel/heartsigniel/model/life/LifeService.java
@@ -3,27 +3,47 @@ package signiel.heartsigniel.model.life;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import signiel.heartsigniel.model.member.Member;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
 @Transactional
-@RequiredArgsConstructor
 public class LifeService {
-    private final LifeRepo lifeRepo;
+    private final LifeRepository lifeRepository;
 
-    public String insert(Long memberId) throws Exception{
-        try {
+    public LifeService(LifeRepository lifeRepository){
+        this.lifeRepository = lifeRepository;
+    }
+
+    public void useLife(Member member){
+
+        // 요청이 들어올때마다
+        deleteExpirationTimeStamp(member.getMemberId());
+
+        // 남은 목숨 있을 경우 목숨 사용
+        if (countRemainingLives(member.getMemberId()) <=1){
             Life life = Life.builder()
-                    .memberId(memberId)
-                    .useDate(LocalDate.now())
+                    .member(member)
                     .build();
 
-            lifeRepo.save(life);
-        } catch (Exception e) {
-            throw new Exception("잘못된 요청입니다.");
+            lifeRepository.save(life);
         }
-        return "OK";
     }
+
+    public Long countRemainingLives(Long memberId){
+        return 2L - lifeRepository.findByMemberIdAndUseDate(memberId, LocalDate.now()).size();
+    }
+
+    public void deleteExpirationTimeStamp(Long memberId) {
+        List<Life> expirationList = lifeRepository.findByMemberIdAndUseDateBefore(memberId, LocalDate.now());
+
+        if (!expirationList.isEmpty()) {
+            lifeRepository.deleteAll(expirationList);
+        }
+    }
+
 }
