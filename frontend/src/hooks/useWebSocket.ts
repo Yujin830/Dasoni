e2223fdb5a8ffc48e18b0c33657abb84a0719b04
zero9657@naver.com <zero9657@naver.com>
2023-08-02 12:52:26 +0900

    fix : websocket 핸들러

--- frontend/src/hooks/useWebSocket.ts
+++ frontend/src/hooks/useWebSocket.ts
@@ -1,9 +1,9 @@
-import * as Stomp from '@stomp/stompjs';
+import { Client, Frame } from '@stomp/stompjs';
 import { useEffect, useState } from 'react';
 
 interface Param {
-  onConnect: (frame: Stomp.Frame, client: Stomp.Client) => void;
-  beforeDisconnected: (frame: Stomp.Frame, client: Stomp.Client) => void;
+  onConnect: (frame: Frame, client: Client) => void;
+  beforeDisconnected: (frame: Frame, client: Client) => void;
   reconnectDelay?: number;
 }
 
@@ -11,27 +11,32 @@ export const useWebSocket = ({ onConnect, beforeDisconnected, reconnectDelay }: 
   const [connected, setConnected] = useState(false);
 
   useEffect(() => {
-    let stompClient: Stomp.Client | undefined = undefined;
-    const config: Stomp.StompConfig = {
-      connectHeaders: {
-        Authorization: `Bearer ${localStorage.getItem('jwtToken')}`,
-      },
-      brokerURL: `ws://localhost:8080/ws`,
-      reconnectDelay: reconnectDelay ? reconnectDelay : 5000,
-      onConnect: (frame) => {
-        console.log('소켓 연결 성공', frame);
-        setConnected(true);
-        onConnect(frame, stompClient!);
-      },
-      onDisconnect: (frame) => {
-        console.log('소켓 연결 끊음', frame);
-        setConnected(false);
-        beforeDisconnected(frame, stompClient!);
-      },
+    const stompClient = new Client({
+      brokerURL: `ws://localhost:8080/ws/chat`,
+      // connectHeaders: {
+      //   Authorization: `Bearer ${localStorage.getItem('jwtToken')}`,
+      // },
       logRawCommunication: false,
+    });
+
+    stompClient.onConnect = (frame) => {
+      console.log('소켓 연결 성공', frame);
+      setConnected(true);
+      onConnect(frame, stompClient!);
     };
 
-    stompClient = new Stomp.Client(config);
+    stompClient.onDisconnect = (frame) => {
+      console.log('소켓 연결 끊음', frame);
+      setConnected(false);
+      beforeDisconnected(frame, stompClient!);
+    };
+
+    stompClient.onStompError = (frame) => {
+      console.log(frame);
+      console.log('Broker reported error: ' + frame.headers['message']);
+      console.log('Additional details: ' + frame.body);
+    };
+
     stompClient.activate();
 
     return () => {
--- frontend/src/page/WatingRoomPage/WatingRoomPage.tsx
+++ frontend/src/page/WatingRoomPage/WatingRoomPage.tsx
@@ -89,23 +89,44 @@ function WaitingRoomPage() {
   const { roomId } = useParams();
   const waitingRoomInfo = useAppSelector((state) => state.waitingRoom);
 
+  //webSocket 연결 테스트
+  const testSocket = new WebSocket('ws://localhost:8080/ws/chat');
+  testSocket.onopen = function (e) {
+    console.log('socket 연결 완료');
+    testSocket.send('My name is Bora');
+  };
+
+  testSocket.onmessage = function (event) {
+    alert(`[message] 서버로부터 전송받은 데이터: ${event.data}`);
+  };
+
+  testSocket.onerror = function (err) {
+    console.log('에러 발생');
+    console.log(err);
+  };
+
+  testSocket.onclose = function (e) {
+    console.log('커넥션 종료');
+    console.log(e);
+  };
+
   // webSocket 사용해 실시간으로 대기방에 입장하는 memberList 갱신
-  useWebSocket({
-    onConnect: (frame, client) => {
-      console.log(frame);
-      client.subscribe(`/topic/room/${roomId}`, (res) => {
-        console.log(res);
-        const result = JSON.parse(res.body);
-        setMemberList(result.members);
-      });
-      client.publish({ destination: `/app/chat/message` });
-    },
-    beforeDisconnected: (frame, client) => {
-      console.log(frame);
-      console.log(client);
-      setMemberList([]);
-    },
-  });
+  // useWebSocket({
+  //   onConnect: (frame, client) => {
+  //     console.log(frame);
+  //     client.subscribe(`/topic/room/${roomId}`, (res) => {
+  //       console.log(res);
+  //       const result = JSON.parse(res.body);
+  //       setMemberList(result.members);
+  //     });
+  //     client.publish({ destination: `/app/room/chat/${roomId}` });
+  //   },
+  //   beforeDisconnected: (frame, client) => {
+  //     console.log(frame);
+  //     console.log(client);
+  //     setMemberList([]);
+  //   },
+  // });
 
   const handleStartBtn = () => {
     alert('미팅이 3초 후 시작됩니다');
