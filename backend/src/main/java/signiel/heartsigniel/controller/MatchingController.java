423786db494ec12e0c52ad577c914fae1c5899f7
rladbwls30 <rladbwls30@naver.com>
2023-08-03 17:59:44 +0900

    Fix: api 수정

--- backend/src/main/java/signiel/heartsigniel/controller/MatchingController.java
+++ backend/src/main/java/signiel/heartsigniel/controller/MatchingController.java
@@ -9,7 +9,7 @@ import signiel.heartsigniel.model.matching.code.MatchingCode;
 import signiel.heartsigniel.model.rating.dto.TotalResultRequest;
 
 @RestController
-@RequestMapping("/api/match")
+@RequestMapping("/match")
 public class MatchingController {
 
     private final MatchingService matchingService;
