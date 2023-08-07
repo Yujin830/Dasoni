006859bc5c9093a29893bc7dd132ee0fff1b05e1
zero9657@naver.com <zero9657@naver.com>
2023-08-07 17:33:06 +0900

    feat : 가이드 기능 개발

---
+++ backend/src/main/java/signiel/heartsigniel/config/WebSocketConfig.java
@@ -0,0 +1,52 @@
+package signiel.heartsigniel.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+/**
+ * 채팅 시스템 개발을 위핸 Config Class
+ */
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+    /**
+     * 소켓 연결과 관련된 설정
+     *
+     * addEndpoint : 소켓 연결 URL
+     * setAllowedOriginPatterns : 소켓 CORS 설정
+     * withSocketJS : 소켓을 지원하지 않는 브라우저라면 socketJs를 사용하도록 설정.
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        
+        registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+
+    /**
+     * STOMP 사용을 위한 MessageBroker 설정
+     *
+     * enableSimpleBroker : 메세지를 받을 때 경로 설정
+     * /queue, /topic을 통해 1:1 , 1:N 설정
+     * 두 API가 Perfix로 붙은 경우 messageBroker가 해당 경로를 가로 챔.
+     *
+     * setApplicationDestinationPrefixes : 메세지를 보낼 때 관련 경로 설정
+     * 클라이언트가 메시지를 보낼 때 경로 앞에 /app이 붙어있으면 Broker로 보내짐.
+     *
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/queue", "/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
