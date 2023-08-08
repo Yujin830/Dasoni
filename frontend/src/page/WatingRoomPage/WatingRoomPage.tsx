6269a8ef9afc6a7c5ee7ab9236648cb0716dda3f
alsskfen1231 <alsskfen1231@gmail.com>
2023-08-08 15:04:37 +0900

    fix

--- frontend/src/page/WatingRoomPage/WatingRoomPage.tsx
+++ frontend/src/page/WatingRoomPage/WatingRoomPage.tsx
@@ -132,7 +132,7 @@ function WaitingRoomPage() {
                 <WaitingMemberBox
                   key={member.memberId}
                   nickname={member.nickname}
-                  point={member.rating}
+                  rating={member.rating}
                   matchCnt={member.meetingCount}
                   gender={member.gender}
                   profileImageSrc={member.profileImageSrc}
@@ -144,7 +144,7 @@ function WaitingRoomPage() {
                 <WaitingMemberBox
                   key={member.memberId}
                   nickname={member.nickname}
-                  point={member.rating}
+                  rating={member.rating}
                   matchCnt={member.meetingCount}
                   gender={member.gender}
                   profileImageSrc={member.profileImageSrc}
