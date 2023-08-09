bb0727e2aae01bbcc5c955ff7a7570a28183954e
alsskfen1231 <alsskfen1231@gmail.com>
2023-08-09 15:02:19 +0900

    feat/FE-370

--- frontend/src/page/WatingRoomPage/WatingRoomPage.tsx
+++ frontend/src/page/WatingRoomPage/WatingRoomPage.tsx
@@ -35,6 +35,8 @@ import convertScoreToName from '../../utils/convertScoreToName';
 // };
 
 function WaitingRoomPage() {
+  const [isLoading, setIsLoading] = useState(true); // 로딩 상태를 관리하는 상태 변수
+
   const waitingRoomInfo = useAppSelector((state) => state.waitingRoom);
   const { gender } = useAppSelector((state) => state.user);
   const [memberList, setMemberList] = useState<WaitingMember[]>(
