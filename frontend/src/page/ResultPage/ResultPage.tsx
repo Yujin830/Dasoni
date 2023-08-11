14073b2e2af13c645784cac6b6248ee713d5de31
KimSeonHui <zero9657@naver.com>
2023-08-12 00:12:01 +0900

    feat : result page UI 개발

--- frontend/src/page/ResultPage/ResultPage.tsx
+++ frontend/src/page/ResultPage/ResultPage.tsx
@@ -3,30 +3,62 @@ import Header from '../../components/Header/Header';
 import Banner from '../../components/Banner/Banner';
 import axios from 'axios';
 import { useAppSelector } from '../../app/hooks';
+import FailResult from '../../components/Result/FailResult/FailResult';
+import SuccessResult from '../../components/Result/SuccessResult/SuccessResult';
+import FilledButton from '../../components/Button/FilledButton';
+import { useNavigate } from 'react-router';
 
 function ResultPage() {
-  const [result, setResult] = useState();
-  const { roomId } = useAppSelector((state) => state.waitingRoom);
-  const { memberId } = useAppSelector((state) => state.user);
+  const [success, setSuccess] = useState(false);
+  const { memberId, profileImageSrc, rating } = useAppSelector((state) => state.user);
+  const { resultOfRoomMember } = useAppSelector((state) => state.meetingRoom);
+  const [otherProfile, setOtherProfile] = useState('');
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    requestResult();
+    setResult();
   }, []);
 
-  const requestResult = async () => {
-    const res = await axios.get(`/api/rooms/${roomId}/members/${memberId}`);
-    console.log(res.data);
+  const setResult = async () => {
+    if (resultOfRoomMember.matchMemberId !== 0) {
+      setSuccess(true);
 
-    if (res.status === 200) {
-      setResult(res.data.content);
+      // TODO : 매칭된 상대방 정보 가져오기
     }
+  };
+
+  const handleClose = () => {
+    console.log('메인으로');
+    navigate('/main', { replace: true });
   };
   return (
     <div id="result">
       <Header />
       <Banner />
       <main>
-        <h2>미팅 결과</h2>
+        <div id="title-box">
+          <h2>매칭 결과</h2>
+          <h3>{success ? '매칭에 성공했습니다!' : '매칭에 실패했습니다...'}</h3>
+        </div>
+        <div id="user-info-box">
+          {success ? (
+            // TODO : 상대방 정보 불러오기
+            <SuccessResult
+              profileSrc={profileImageSrc}
+              rating={rating}
+              otherProfileSrc={profileImageSrc}
+              otherRating={rating}
+            />
+          ) : (
+            <FailResult profileSrc={profileImageSrc} />
+          )}
+        </div>
+        <h3 className="result-rating">{resultOfRoomMember.ratingChange}point</h3>
+        <div className="buttons">
+          {success && <button>연장하기</button>}
+          <FilledButton content="메인으로" classes="to-main-btn" handleClick={handleClose} />
+        </div>
       </main>
     </div>
   );
