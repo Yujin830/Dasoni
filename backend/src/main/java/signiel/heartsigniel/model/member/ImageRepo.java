4642ee64f67a9044ceaa4ab65f6363c0dc8227f0
hyeonwoo <hjk8596@naver.com>
2023-08-11 14:28:36 +0900

    image upload

--- backend/src/main/java/signiel/heartsigniel/model/member/ImageRepo.java
+++ backend/src/main/java/signiel/heartsigniel/model/member/ImageRepo.java
@@ -3,4 +3,6 @@ package signiel.heartsigniel.model.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepo extends JpaRepository<Image, Long> {
+    Image findImageByStoredName(String filename);
+    Image findImageByAccessUrl(String url);
 }
