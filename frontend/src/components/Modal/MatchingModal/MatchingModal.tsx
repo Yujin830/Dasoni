86845ccedf70ca0026a9fa54ac4c06bc1d519e98
27kanghan <27kanghan@naver.com>
2023-08-13 22:57:30 +0900

    feat: 빠른 매칭 시스템 구현 완료, 6번째 사람 미팅 안들어가지는  버그 수정해야함.

--- frontend/src/components/Modal/MatchingModal/MatchingModal.tsx
+++ frontend/src/components/Modal/MatchingModal/MatchingModal.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './MatchingModal.css';
 import axios from 'axios';
 import { useAppSelector } from '../../../app/hooks';
+import { useNavigate } from 'react-router';
 
 interface MatchingModalProps {
   onClose: () => void;
@@ -9,8 +10,42 @@ interface MatchingModalProps {
 
 const MatchingModal: React.FC<MatchingModalProps> = ({ onClose }) => {
   const member = useAppSelector((state) => state.user);
-  const quick = axios.get(`api/alarm/subscribe/${member.memberId}`);
-  console.log('123' + quick);
+  const navigate = useNavigate();
+  const eventSource = new EventSource(`api/alarm/subscribe/${member.memberId}`);
+  console.log('멤버아이디 ', member.memberId);
+
+  eventSource.addEventListener('match', (event) => {
+    const { data: receivedConnectData } = event;
+    console.log('connect!!', receivedConnectData);
+
+    const res = event.data;
+    const parseData = JSON.parse(res);
+    console.log(parseData.roomId);
+    console.log(parseData.status);
+    console.log(parseData.readyState);
+
+    if (parseData.status === 'OK') {
+      console.log('Matched Room ID:', parseData.roomId);
+      alert(`매칭 완료! 방 ID: ${parseData.roomId}. 3초 후에 방으로 이동합니다.`);
+
+      setTimeout(() => {
+        navigate(`/meeting/${parseData.roomId}`, { replace: true });
+        eventSource.close();
+        onClose();
+      }, 3000);
+    } else {
+      console.log(res.message);
+    }
+    console.log(eventSource.readyState);
+  });
+
+  eventSource.onopen = () => {
+    console.log('onopen', eventSource.readyState);
+  };
+
+  eventSource.onerror = (event: any) => {
+    console.error('SSE error:', event);
+  };
 
   const handleCancel = async () => {
     try {
@@ -21,6 +56,12 @@ const MatchingModal: React.FC<MatchingModalProps> = ({ onClose }) => {
       console.error('Error canceling the matching:', error);
     }
   };
+
+  useEffect(() => {
+    return () => {
+      eventSource.close(); // 컴포넌트 언마운트 시 SSE 연결 종료
+    };
+  }, []);
 
   return (
     <div className="openroommodal-overlay active">
