349b1555318293f82c8e494913bfba19f7485348
KimSeonHui <zero9657@naver.com>
2023-08-13 22:50:10 +0900

    feat : 중간 퇴장 시 라이프 감소

--- frontend/src/components/ToolBar/ToolBar.tsx
+++ frontend/src/components/ToolBar/ToolBar.tsx
@@ -3,6 +3,8 @@ import './ToolBar.css';
 import { useNavigate } from 'react-router';
 import axios from 'axios';
 import { useAppSelector } from '../../app/hooks';
+import { setRemainLife } from '../../app/slices/user';
+import { useDispatch } from 'react-redux';
 
 interface ToolBarProps {
   onChangeCameraStatus: (status: boolean) => void;
@@ -14,7 +16,9 @@ function ToolBar({ onChangeCameraStatus, onChangeMicStatus }: ToolBarProps) {
   const [cameraStatus, setCameraStatus] = useState(true);
   const navigate = useNavigate();
   const { roomId } = useAppSelector((state) => state.waitingRoom);
-  const { memberId } = useAppSelector((state) => state.user);
+  const { memberId, remainLife } = useAppSelector((state) => state.user);
+
+  const dispatch = useDispatch();
 
   const handleMicStatus = () => {
     setMicStatus((prev) => !prev);
@@ -26,6 +30,8 @@ function ToolBar({ onChangeCameraStatus, onChangeMicStatus }: ToolBarProps) {
 
   const handleExitBtn = async () => {
     if (confirm('미팅 중 퇴장 시 패널티를 받습니다.\n정말 나가시겠습니까?')) {
+      if (remainLife !== undefined) dispatch(setRemainLife(remainLife - 1)); // 라이프 감소
+      await axios.delete(`/api/rooms/${roomId}/members/${memberId}`);
       navigate('/result', { replace: true });
     }
   };
