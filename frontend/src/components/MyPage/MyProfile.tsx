e2da5bec53fe43e17088d647b2835c536182dcc0
alsskfen1231 <alsskfen1231@gmail.com>
2023-08-14 12:46:09 +0900

    feat/FE-411

--- frontend/src/components/MyPage/MyProfile.tsx
+++ frontend/src/components/MyPage/MyProfile.tsx
@@ -11,6 +11,7 @@ function MyProfile({ setType }: { setType: (type: string) => void }) {
   const { loginId, nickname, job, birth, siDo, guGun, profileImageSrc } = useAppSelector(
     (state) => state.user,
   );
+  console.log(siDo, guGun);
   const user = useAppSelector((state) => state.user);
   // TODO : 최근 매칭된 다소니 리스트 조회 recentUserList로 state 변경
   const [recentUserList, setRecentUserList] = useState([]);
