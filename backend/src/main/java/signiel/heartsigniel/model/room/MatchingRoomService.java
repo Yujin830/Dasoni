93f2b674603679278ad1009aee7fa321b18bae18
이명우 <dlauddndlek@naver.com>
2023-08-16 14:26:44 +0900

    feat: 방 결과 계산 API 처리율 제한 로직 추가 #BACK-451 Resolves : #451

--- backend/src/main/java/signiel/heartsigniel/model/room/MatchingRoomService.java
+++ backend/src/main/java/signiel/heartsigniel/model/room/MatchingRoomService.java
@@ -79,7 +79,7 @@ public class MatchingRoomService {
 
     public Response enqueueRoom(Long roomId) {
         // Redis에서 해당 roomId의 요청이 진행 중인지 확인
-        Boolean isAbsent = redisTemplate.opsForValue().setIfAbsent("room_request_" + roomId, 0L, 10, TimeUnit.SECONDS);
+        Boolean isAbsent = redisTemplate.opsForValue().setIfAbsent("room_enqueue_request_" + roomId, 0L, 10, TimeUnit.SECONDS);
 
         if (Boolean.TRUE.equals(isAbsent)) {
             // 중복 요청이 아님. 처리 계속 진행
